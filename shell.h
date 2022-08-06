0fb43c3d2457503a402d2766ee4541a58d7fb131
Nganga <eliud.gachunji@gmail.com>
2022-08-06 04:24:07 -0700

    creatign header file

---
+++ shell.h
@@ -0,0 +1,33 @@
+#ifndef SHELL_H
+
+#define SHELL_H
+
+extern char **environ;
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <sys/wait.h>
+
+#include <stdlib.h>
+
+#include <unistd.h>
+
+#include <sys/stat.h>
+
+#include <sys/types.h>
+
+void print_environ(void);
+
+int _strcmp(char *s1, char *s2);
+
+char **parse_input_string(char *buffer);
+
+char *path_search(char *cmd);
+
+char *_strncat(char *dest, char *src, int n)
+
+#endif
