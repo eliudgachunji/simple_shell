8ab4d10f6be105a6d67a6a79b3ad6a9f17893bc8
Nganga <eliud.gachunji@gmail.com>
2022-08-09 01:30:51 -0700

    function to print environment variables

---
+++ print_environ.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * print_environ - prints all the environment variables.
+ * Return: nothing.
+ */
+
+void print_environ(void)
+
+{
+
+int i;
+
+i = 0;
+
+while (environ[i])
+
+printf("%s\n", environ[i++]);
+}
